647171317340927d7376b1d3d7ea98305e5b38f8
Marksolidor <marco350sl@gmail.com>
2022-07-31 10:37:43 -0400

    feat tasks

---
+++ assets/java/script.js
@@ -0,0 +1,5 @@
+const tasks = [
+    { id: 1, task: "Danza y ejercicio", state: false },
+    { id: 2, task: "Solucionar la hambruna mundial", state: false },
+    { id: 3, task: "Cena conmigo", state: false },
+  ];
